c7320856b495bade87b92a2e3df8739507b31702
Alexander Gamble <alexander.gamble@outlook.com>
2013-12-12 13:27:25 +0000

    added first revision of databasing

---
+++ mango/database.py
@@ -0,0 +1,36 @@
+from mongoengine import *
+import json, datetime
+
+class Alarm(Document):
+    name = StringField(required=True)
+    time = DateTimeField(required=True)
+    activated = BooleanField(default=True)
+
+def returnJsonFromRaw(rawJson):
+    raw_data = json.loads(rawJson)
+    if not raw_data:
+        print "No data was sent"
+        return
+    if not (raw_data['name'] and raw_data['time']):
+        print "The fields either could not be parsed, or one of them is missing"
+        return
+    return  raw_data
+    
+def writeJsonToDatabase(obj):
+    alarm = Alarm(name=obj['name'], time=obj['time'])
+    alarm.save()
+
+def getAlarms():
+    return Alarm.objects
+
+def getTodayAlarms():
+    yesterday = Date.today() - timedelta(1)
+    return Alarm.objects(time__gt=yesterday)
+
+def activateAlarm(name):
+    alarm = Alarm.objects(name__exact=name)
+    alarm.activated = True
+    alarm.save()
+    print "Alarm " + name + " saved."
+    return
+    
